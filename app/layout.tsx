48a17035c2a916150d0b031e442b3bc4c2bcc3dd
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-08 10:40:21 +0100

    Created app w/ Nextjs, React, TypeScript, Tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ART-ificial",
+  description:
+    "Built with Next.js, React.js, TailwindCSS, TypeScript, Microsoft Azure and ChatGPT",
+  icons: {
+    icon: "/icon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
