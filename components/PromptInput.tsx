1a8f99fa964c9cab7112477c1d627db1a8b2fac7
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-08 14:24:14 +0100

    Logic and styling for search box and buttons

---
+++ components/PromptInput.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+const PromptInput = () => {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="m-10">
+      <form className="flex flex-col lg:flex-row shadow-md shadow-slate-400/10 border rounded-md lg:divide-x">
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter your creative idea..."
+          className="flex-1 p-4 outline-none rounded-md"
+        />
+        <button
+          type="submit"
+          disabled={!input}
+          className={`p-4 font-bold ${
+            input
+              ? "bg-violet-500 text-white transition-colors duration-200"
+              : "text-gray-300 cursor-not-allowed"
+          }`}
+        >
+          Generate Image
+        </button>
+        <button
+          type="button"
+          className="p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400"
+        >
+          Use Suggestion
+        </button>
+        <button
+          type="button"
+          className="p-4 bg-white text-violet-500 border-none transition-colors duration-200 rounded-b-md md:rounded-r-md md:rounded-bl-none font-bold"
+        >
+          New Suggestion
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default PromptInput;
