ca4c965141dc454495c66cec7f99c76a41ac4917
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-10 18:21:38 +0100

    Whitelisted image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["artificialaiimageg698f6c.blob.core.windows.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
