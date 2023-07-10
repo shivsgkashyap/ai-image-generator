05daf0c92706d12ee048085794ef6000ed70401b
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-07-10 16:15:05 +0100

    POST request to send prompt to OpenAI to gen image

---
+++ azure/src/functions/generateImage.js
@@ -0,0 +1,37 @@
+const { app } = require("@azure/functions");
+const openai = require("../../lib/openai");
+const axios = require("axios");
+const generateSASToken = require("../../lib/generateSASToken");
+
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const accountName = process.env.accountName;
+
+const containerName = "images";
+
+app.http("generateImage", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: async (request) => {
+    const { prompt } = await request.json();
+
+    console.log(`Prompt is ${prompt}`);
+
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    image_url = response.data.data[0].url;
+
+    //Download the image and return it as an arrayBuffer
+    const res = await axios.get(image_url, { responseType: "arraybuffer" });
+
+    const arraybuffer = res.data;
+
+    sas;
+
+    return { body: sasToken };
+  },
+});
